94ee8a90f0e16e2daaad144f391ddddf7d057523
t-tarinskaya <t-tarinskaya@yandex-team.ru>
2026-08-13 15:23:05 +0300

    Add therapy conversation phrases

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='phrase-prototype-v10';
+const CACHE='phrase-prototype-v11';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
