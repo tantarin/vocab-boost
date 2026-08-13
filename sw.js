3370c864a8abbc712b2c74464aad564a1cc52ef8
t-tarinskaya <t-tarinskaya@yandex-team.ru>
2026-08-13 13:58:55 +0300

    Refine office collection with advanced vocabulary

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='phrase-prototype-v5';
+const CACHE='phrase-prototype-v6';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request))));
